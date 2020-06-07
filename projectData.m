47fa30b3af72c9829e9a1772e0da544e73a5c540
Xiaocong(Bill) Yan <50472308+Xiaocong233@users.noreply.github.com>
2020-06-06 22:58:39 -0700

    Update projectData.m

--- projectData.m
+++ projectData.m
@@ -2,22 +2,13 @@
 % on to the top k eigenvectors
 function Z = projectData(X, U, K)
   
-% You need to return the following variables correctly.
-Z = zeros(size(X, 1), K);
+  % You need to return the following variables correctly.
+  Z = zeros(size(X, 1), K);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the projection of the data using only the top K 
-%               eigenvectors in U (first K columns). 
-%               For the i-th example X(i,:), the projection on to the k-th 
-%               eigenvector is given as follows:
-%                    x = X(i, :)';
-%                    projection_k = x' * U(:, 1:k);
-%
+  % Obtain eigenvector vector with reduced dimensions
+  Ureduced = U(:, 1:K);
 
-% Obtain eigenvector vector with reduced dimensions
-Ureduced = U(:, 1:K);
-
-% Compute for z, projection vector
-Z = X * Ureduced;
+  % Compute for z, projection vector
+  Z = X * Ureduced;
 
 end
